34177deeb42576063d423f2537307e6e66ec0c80
nguyenhuutoan05697 <nguyenhuutoan05697@gmail.com>
2021-03-17 09:58:26 +0700

    1st upload to Github

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author HuuToan
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello to Github");
+        System.out.println("This is 1St upload connects to remote Github server");
+    }
+    
+}
